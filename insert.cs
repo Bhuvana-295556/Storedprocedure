8b335c7202649cb2fff1ca2a4688eddf2c734a95
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-09-15 19:19:06 +0530

    Update insert.cs

--- insert.cs
+++ insert.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace DBConnectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conObj = new SqlConnection(@"data source = (localdb)\ProjectsV13;database=AdventureWorks2012;integrated security=SSPI");
+            SqlCommand queryObj = new SqlCommand(@"EXEC usp_InsertData 'Sales and marketing', 'Manufacturing', '2021-09-15'", conObj);
+            conObj.Open();
+            queryObj.ExecuteNonQuery();
+            conObj.Close();
+        }
+    }
+}
